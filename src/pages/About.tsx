5d589eb6d2df3f90d011ba2849cf6150843ee4b7
Ilya Kruchinin <kr4chinin@mail.ru>
2022-07-20 20:30:47 +0300

    feat: about page content

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import Navbar from '../components/navbar/Navbar'
 import { useLogout } from '../hooks/useLogout'
 import './styles/About.scss'
@@ -10,18 +11,113 @@ const About = () => {
 			<Navbar actionButtonTitle="👋🏼 Log out" actionHandler={logout} />
 			<div className="about-container">
 				<div className="box content">
-					<p className="title">Information header</p>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ex
-						deserunt ipsa vero, eaque voluptatum molestiae praesentium maiores
-						maxime asperiores obcaecati aperiam harum aut consequuntur atque
-						quod? Ad, facilis! Amet optio nam soluta perferendis ut dignissimos
-						pariatur natus excepturi voluptatem dicta, tempora ad similique
-						nihil cupiditate! Vero id ducimus magnam. Lorem ipsum dolor, sit
-						amet consectetur adipisicing elit. Cum dignissimos qui omnis.
-						Deleniti, nobis enim ex quos similique at vitae magnam quam veniam.
-						Rerum velit harum at officia laborum numquam.
-					</p>
+					<p className="title">Information about project</p>
+
+					<p className="title is-4">Data sources</p>
+					<article className="message is-medium">
+						<div className="message-body">
+							Feed data fetched randomly from{' '}
+							<a href="https://api.nasa.gov/" target="_blank" rel="noreferrer">
+								Nasa Open API
+							</a>{' '}
+							and presented in format of <b>infinite feed</b>. When you scroll
+							down to the end of the chunk (1 chunk - 10 posts) - another chunk
+							will be loaded automatically. <b>APOD</b> stands for Astronomy
+							Picture Of the Day.
+						</div>
+					</article>
+
+					<p className="title is-4">Functionality</p>
+					<article className="message is-medium">
+						<div className="message-body">
+							This application has:
+							<ul>
+								<li>
+									<b>JWT</b> authorization (backend is also a part of this
+									project, server is hosted on{' '}
+									<a
+										href="https://dashboard.heroku.com/apps"
+										target="_blank"
+										rel="noreferrer"
+									>
+										Heroku
+									</a>
+									, here you can find server{' '}
+									<a
+										href="https://nasa-apod-project-backend.herokuapp.com"
+										target="_blank"
+										rel="noreferrer"
+									>
+										link
+									</a>{' '}
+									and GitHub repo{' '}
+									<a
+										href="https://github.com/kr4chinin/nasa-apod-backend"
+										target="_blank"
+										rel="noreferrer"
+									>
+										link
+									</a>
+								</li>
+								<li>Infinite feed</li>
+								<li>
+									Responsive layout and <b>hamburger</b> menu for mobile devices
+								</li>
+								<li>
+									<Link to="/favourites">Favourites section</Link> where you can
+									save posts which you've liked. Their identificators are stored
+									in the DB so they will be automatically refetched even if you
+									will close this page and come back later
+								</li>
+								<li>
+									<b>Show more</b> / <b>show less</b> buttons for long posts
+								</li>
+							</ul>
+						</div>
+					</article>
+
+					<p className="title is-4">Tech stack</p>
+					<article className="message is-medium">
+						<div className="message-body">
+							In this project I've used following <b>technologies</b>:
+							<ul>
+								<li>React + Typescript</li>
+								<li>ReactQuery (useQuery, useMutation)</li>
+								<li>Usehooks TS (useIntersectionObserver)</li>
+								<li>Bulma + SCSS for interface and layout</li>
+								<li>Axios</li>
+							</ul>
+						</div>
+					</article>
+
+					<p className="title is-4">Conclusion</p>
+					<article className="message is-medium">
+						<div className="message-body">
+							For more information check <b>README</b> files in my GitHub repos:
+							<ol>
+								<li>
+									Frontend -{' '}
+									<a
+										href="https://github.com/kr4chinin/nasa-apod-frontend"
+										target="_blank"
+										rel="noreferrer"
+									>
+										nasa-apod-frontend
+									</a>
+								</li>
+								<li>
+									Backend -{' '}
+									<a
+										href="https://github.com/kr4chinin/nasa-apod-backend"
+										target="_blank"
+										rel="noreferrer"
+									>
+										nasa-apod-backend
+									</a>
+								</li>
+							</ol>
+						</div>
+					</article>
 				</div>
 			</div>
 		</>
